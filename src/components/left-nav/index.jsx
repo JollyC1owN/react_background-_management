61a2bedda5307df7befe2f596814cae36139bded
tomb <tomb@qq.com>
2019-08-13 17:06:34 +0800

    change16

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,6 +1,6 @@
 /* admin左侧导航组件 */
 import React, { Component } from 'react'
-import { Link ,withRouter} from "react-router-dom"
+import { Link, withRouter } from "react-router-dom"
 import { Menu, Icon } from 'antd';
 
 import "./index.less"
@@ -12,52 +12,15 @@ import menuList from '../../config/menuConfig';
 
 const { SubMenu } = Menu;
 class LeftNav extends Component {
+
 	/* reduce方法实现 */
-		getMenuNodes2 = (menuList) => {
-			return menuList.reduce((pre, item) => {
-				//可能想pre中添加 <Menu.Item>
-				if (!item.children) {
-					pre.push(
-						<Menu.Item key={item.key}>
-							<Link to={item.key}>
-								<Icon type={item.icon} />
-								<span>{item.title}</span>
-							</Link>
-						</Menu.Item>
-					)
-				} else {
-					
-
-
-					pre.push(
-						<SubMenu
-							key={item.key}
-							title={
-								<span>
-									<Icon type={item.icon} />
-									<span>{item.title}</span>
-								</span>
-							}
-						>
-							{
-								//通过递归的方式再创建下面的item
-								this.getMenuNodes2(item.children)
-							}
-						</SubMenu>
-					)
-				 }
-				//也有可能添加<SubMenu>
-				return pre
-			 },[])
-		 } 
-
-
-	/* 根据指定的menu数据生成<Menu.Item>和<SubMenu>的数组 */
-	// 通过map加工来创建标签
-/* 	getMenuNodes = (menuList) => {
-		return menuList.map((item) => {
+	getMenuNodes2 = (menuList) => {
+		//请求的路径
+		const path = this.props.location.pathname
+		return menuList.reduce((pre, item) => {
+			//可能想pre中添加 <Menu.Item>
 			if (!item.children) {
-				return (
+				pre.push(
 					<Menu.Item key={item.key}>
 						<Link to={item.key}>
 							<Icon type={item.icon} />
@@ -65,27 +28,74 @@ class LeftNav extends Component {
 						</Link>
 					</Menu.Item>
 				)
+			} else {
+				//判断当前key是否是我需要的openKey
+				//查找item的所有children中的cItem的key，看是否有一个跟请求的path匹配
+				const cItem = item.children.find(cItem => cItem.key === path)
+				if (cItem) { 
+					this.openKey = item.key
+				}
+				pre.push(
+					<SubMenu
+						key={item.key}
+						title={
+							<span>
+								<Icon type={item.icon} />
+								<span>{item.title}</span>
+							</span>
+						}
+					>
+						{
+							//通过递归的方式再创建下面的item
+							this.getMenuNodes2(item.children)
+							
+						}
+					</SubMenu>
+				)
 			}
-			return (
-				<SubMenu
-					key={item.key}
-					title={
-						<span>
-							<Icon type={item.icon} />
-							<span>{item.title}</span>
-						</span>
-					}
-				>
-					{
-						//通过递归的方式再创建下面的item
-						this.getMenuNodes(item.children)
-					}
-				</SubMenu>
-			)
-		})
-	} */
+			//也有可能添加<SubMenu>
+			return pre
+		}, [])
+	}
+
+
+	/* 根据指定的menu数据生成<Menu.Item>和<SubMenu>的数组 */
+	// 通过map加工来创建标签
+	/* 	getMenuNodes = (menuList) => {
+			return menuList.map((item) => {
+				if (!item.children) {
+					return (
+						<Menu.Item key={item.key}>
+							<Link to={item.key}>
+								<Icon type={item.icon} />
+								<span>{item.title}</span>
+							</Link>
+						</Menu.Item>
+					)
+				}
+				return (
+					<SubMenu
+						key={item.key}
+						title={
+							<span>
+								<Icon type={item.icon} />
+								<span>{item.title}</span>
+							</span>
+						}
+					>
+						{
+							//通过递归的方式再创建下面的item
+							this.getMenuNodes(item.children)
+						}
+					</SubMenu>
+				)
+			})
+		} */
 
 	render() {
+		// 因为下面要读取openKey，所以要先执行然后在读取
+		const menuNodes = this.getMenuNodes2(menuList)
+
 		//当前请求的路由路径
 		// 由于当前组件不是用作路由组件来使用，所以this.props是一个空对象{ } ，我们借助路由库提供的 withRouter()来添加路由组件该有的三个属性
 		const selectKey = this.props.location.pathname
@@ -98,14 +108,11 @@ class LeftNav extends Component {
 				</Link>
 				<Menu
 					defaultSelectedKeys={[selectKey]}  //默认选中的那个
-					defaultOpenKeys={["/products"]}
+					defaultOpenKeys={[this.openKey]}
 					mode="inline"				//展开和收缩的方式
 					theme="dark"				//主题
 				>
-					{
-						this.getMenuNodes2(menuList)
-					}
-					
+					{	menuNodes}
 					{/* <Menu.Item key="/home">
 						<Link to="/home">
 							<Icon type="home" />
@@ -146,7 +153,7 @@ class LeftNav extends Component {
 export default withRouter(LeftNav)
 
 
-/* 
+/*
 两个小问题
 
 1、 默认选中对应的menuItem
